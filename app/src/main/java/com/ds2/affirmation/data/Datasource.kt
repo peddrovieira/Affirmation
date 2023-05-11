88879816b02b94a073f2296722e30e4e3ff114d8
Peddro Vieira <pe.bonangelo@gmail.com>
2023-05-11 13:51:16 -0300

    Card incluso

--- app/src/main/java/com/ds2/affirmation/data/Datasource.kt
+++ app/src/main/java/com/ds2/affirmation/data/Datasource.kt
@@ -7,13 +7,13 @@ class Datasource {
 
     fun loadAffirmations(): List<Affirmation>{
         return listOf<Affirmation> (
-            Affirmation(R.string.Affirmation1),
-            Affirmation(R.string.Affirmation2),
-            Affirmation(R.string.Affirmation3),
-            Affirmation(R.string.Affirmation4),
-            Affirmation(R.string.Affirmation5),
-            Affirmation(R.string.Affirmation6),
-            Affirmation(R.string.Affirmation7)
+            Affirmation(R.string.Affirmation1, R.drawable.image1),
+            Affirmation(R.string.Affirmation2, R.drawable.image2),
+            Affirmation(R.string.Affirmation3, R.drawable.image3),
+            Affirmation(R.string.Affirmation4, R.drawable.image4),
+            Affirmation(R.string.Affirmation5, R.drawable.image5),
+            Affirmation(R.string.Affirmation6, R.drawable.image6),
+            Affirmation(R.string.Affirmation7, R.drawable.image7)
         )
     }
 }
--- app/src/main/java/com/ds2/affirmation/model/Affirmation.kt
+++ app/src/main/java/com/ds2/affirmation/model/Affirmation.kt
@@ -1,5 +1,9 @@
 package com.ds2.affirmation.model
 
-data class Affirmation(val stringResourceId: Int) {
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import java.sql.RowId
+
+data class Affirmation(@StringRes val stringResourceId: Int,@DrawableRes  val imageResourceId: Int) {
 
 }
